62fd9c8ea6ae626471206452dba997c80a756547
Your Real Name <surafelmuhabaw17@gmail.com>
2025-10-05 13:20:49 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -138,7 +138,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(), // start with login screen 
+      home: LoginScreen(), // start with login screen kd
     );
   }
 }
