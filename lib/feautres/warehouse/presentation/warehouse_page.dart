2d30f804d87ab98363b354b2f34e6efd124fcabd
Your Real Name <surafelmuhabaw17@gmail.com>
2025-10-07 10:16:14 +0300

    update

--- lib/feautres/warehouse/presentation/warehouse_page.dart
+++ lib/feautres/warehouse/presentation/warehouse_page.dart
@@ -11,16 +11,45 @@ class _WarehousePageState extends State<WarehousePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // title: const Text("My App"),
+        backgroundColor: Colors.white,
         title: SizedBox(
           width: double.infinity,
+          height: 40,
           child: TextField(
-              decoration: InputDecoration(
-                  hintText: 'Search Items',
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none))),
+            decoration: InputDecoration(
+              hintText: 'Search Items',
+              prefixIcon: Icon(Icons.search, color: Colors.grey),
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(color: Colors.grey, width: 1),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(color: Colors.blue, width: 1.5),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround, // optional
+
+              children: [
+                Icon(Icons.filter_alt),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(Icons.shop)
+              ],
+            )
+          ],
         ),
       ),
     );
