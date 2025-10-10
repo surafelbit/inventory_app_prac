4f6788aa196c49a658fa6640cffb49775ef07d3c
Your Real Name <surafelmuhabaw17@gmail.com>
2025-10-10 08:57:52 +0300

    update

--- lib/feautres/profile/presentation/admin_profile_screen.dart
+++ lib/feautres/profile/presentation/admin_profile_screen.dart
@@ -229,7 +229,7 @@ class _AdminProfileScreenState extends ConsumerState<AdminProfileScreen> {
                                                           const SizedBox(
                                                               height: 20),
 
-                                                          // ✅ Selectable buttons
+                                                          // ✅ Selectable buttonss
                                                           Row(
                                                             mainAxisAlignment:
                                                                 MainAxisAlignment
