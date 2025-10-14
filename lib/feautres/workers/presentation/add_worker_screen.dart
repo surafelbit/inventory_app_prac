376c21b75c4e3df00c6958c0762ac11a64a20fdf
Your Real Name <surafelmuhabaw17@gmail.com>
2025-10-14 22:08:34 +0300

    i had a dream i could by my way to heaven

--- lib/feautres/workers/presentation/add_worker_screen.dart
+++ lib/feautres/workers/presentation/add_worker_screen.dart
@@ -17,7 +17,63 @@ class _AddWorkerScreenState extends ConsumerState<AddWorkerScreen> {
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           // handle button press here
+          showBottomSheet(
+              context: context,
+              backgroundColor: Colors.white,
+              // isScrollControlled: true,
+              builder: (BuildContext modalContext) {
+                return DefaultTabController(
+                    length: 3,
+                    child: StatefulBuilder(builder:
+                        (BuildContext context, StateSetter setModalState) {
+                      return Padding(
+                        padding: EdgeInsets.only(
+                          bottom:
+                              MediaQuery.of(modalContext).viewInsets.bottom +
+                                  10,
+                          left: 20,
+                          right: 20,
+                          top: 15,
+                        ),
+                        child: Column(
+                          children: [Text('Stronger')],
+                        ),
+                      );
+                    }));
+              });
         },
+//         onPressed: () {
+//   showModalBottomSheet(
+//     context: context,
+//     backgroundColor: Colors.white,
+//     isScrollControlled: true, // makes it slide higher when keyboard opens
+//     shape: const RoundedRectangleBorder(
+//       borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+//     ),
+//     builder: (BuildContext modalContext) {
+//       return Padding(
+//         padding: EdgeInsets.only(
+//           bottom: MediaQuery.of(modalContext).viewInsets.bottom + 10,
+//           left: 20,
+//           right: 20,
+//           top: 15,
+//         ),
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min, // shrink to fit content
+//           children: const [
+//             Text(
+//               'Add Worker Details',
+//               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//             ),
+//             SizedBox(height: 10),
+//             Text('This is your modal content area.'),
+//           ],
+//         ),
+//       );
+//     },
+//   );
+// },
+
         icon: const Icon(
           Icons.person_add_alt_1, // looks better for “add worker”
           color: Colors.white,
