08b4912741fae175b50039dd3a6827405fb9b31d
Your Real Name <surafelmuhabaw17@gmail.com>
2025-10-14 22:09:57 +0300

    update

--- lib/feautres/workers/presentation/add_worker_screen.dart
+++ lib/feautres/workers/presentation/add_worker_screen.dart
@@ -46,7 +46,7 @@ class _AddWorkerScreenState extends ConsumerState<AddWorkerScreen> {
 //   showModalBottomSheet(
 //     context: context,
 //     backgroundColor: Colors.white,
-//     isScrollControlled: true, // makes it slide higher when keyboard opensld
+//     isScrollControlled: true, // makes it slide higher when keyboard opens
 //     shape: const RoundedRectangleBorder(
 //       borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
 //     ),
