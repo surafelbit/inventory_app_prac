b737865dfc3b49ba9d2f0e2f4a6254ade1457e39
Your Real Name <surafelmuhabaw17@gmail.com>
2025-10-19 00:21:55 +0300

    update

--- lib/feautres/workers/presentation/add_worker_screen.dart
+++ lib/feautres/workers/presentation/add_worker_screen.dart
@@ -489,7 +489,7 @@ class _AddWorkerScreenState extends ConsumerState<AddWorkerScreen> {
                                 },
                               );
                             } catch (error) {
-                              // Close the loading dialog in case of error
+                              // Close the loading dialog in case of errord
                               Navigator.of(modalContext).pop();
                               print(error);
                               // Show error message
