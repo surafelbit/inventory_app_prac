a6c2774dc648a109c70061b1f09e8495778f7516
Your Real Name <surafelmuhabaw17@gmail.com>
2025-10-28 11:28:26 +0300

    somechange

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(), // start with login screen
+      home: LoginScreen(), // starts with login screen
       // home: ProfileScreen(),
       //home: const WelcomeScreen(),
       // home: AdminProfileScreen(),
